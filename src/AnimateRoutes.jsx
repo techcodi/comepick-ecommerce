ab317c7b8c65fbc17216374b48cd2196c9e42cd5
Josh0011 <joshlokosu18@gmail.com>
2025-05-27 13:36:26 +0100

    fix error

--- src/AnimateRoutes.jsx
+++ src/AnimateRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./userDashboard/components/Home";
-import UserLogin from "./userDashboard/components/userLogin";
+import UserLogin from "./userDashboard/components/UserLogin";
 import UserSignup from "./userDashboard/components/userSignup";
 import ProtectedRoute from "./userDashboard/context/ProtectedRoute";
 import CategoryList from "./userDashboard/features/Products/CategoryList";
